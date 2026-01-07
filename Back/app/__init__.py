476fae7a30921261361480f101794139e88c7df9
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2026-01-07 00:54:36 -0300

    atualizando __init__.py

--- Back/app/__init__.py
+++ Back/app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from Back.app.routes import bp
+    from app.routes import bp
     app.register_blueprint(bp)
     return app
+
+
+
