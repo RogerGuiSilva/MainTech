c1786570b86e2f965cfc0cd529e02da65dc729bb
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2026-01-08 21:25:12 -0300

    Criando  GET e POST

--- Back/app/routes.py
+++ Back/app/routes.py
@@ -1,6 +1,18 @@
-from flask import Blueprint, jsonify
+
+from flask import Blueprint, jsonify, request
 bp = Blueprint ('main', __name__)
 
 @bp.route("/test", methods=["GET"])
 def test():
-    return jsonify({"message": "back ta rodando"})
+    return jsonify({
+
+        "status": "ok",
+        "message": "Eba"
+        })
+
+@bp.route("/echo", methods=["POST"])
+def echo():
+    dados = request.get_json()
+    return jsonify ({
+        "recebido": dados
+    })
