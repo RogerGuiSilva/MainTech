2365b2d3e0be19fb8a308322c2aa303f50ec0a11
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2026-01-11 11:15:23 -0300

    Inicialização do banco SQLite do projeto

---
+++ Back/app/dataBase/db.py
@@ -0,0 +1,33 @@
+import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "database", "app.db")
+
+def get_connection():
+    conn = sqlite3.connect(DB_PATH)
+    return conn
+
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS equipamentos 
+               (id INTEGER PRIMARY KEY AUTOINCREMENT,
+               nome TEXT NOT NULL,
+               setor TEXT NOT NULL,
+               tipo TEXT NOT NULL,
+               status TEXT NOT NULL)
+               
+               
+               
+               
+               """)
+
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+        init_db()
+        print("ok")
