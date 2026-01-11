e0cec0ab1950d16c3e296f6193d2e8ffbe1aac50
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2026-01-11 20:47:41 -0300

    adicionando os equipamentos e ferramentas

--- Back/app/routes.py
+++ Back/app/routes.py
@@ -1,18 +1,51 @@
-
 from flask import Blueprint, jsonify, request
-bp = Blueprint ('main', __name__)
+from app.dataBase.db import get_connection 
 
-@bp.route("/test", methods=["GET"])
-def test():
-    return jsonify({
+bp = Blueprint('main', __name__)
 
-        "status": "ok",
-        "message": "Eba"
+@bp.route("/equipamentos", methods=["POST"])
+def criar_equipamentos():
+    dados = request.get_json()  
+
+    if not dados:
+        return jsonify({"erro": "JSON não enviado"}), 400
+
+    
+    campos_obrigatorios = ["nome", "setor", "tipo", "status"]
+    for campo in campos_obrigatorios:
+        if campo not in dados or not dados[campo]:
+            return jsonify({"erro": f"Campo '{campo}' é obrigatório"}), 400
+
+   
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO equipamentos (nome, setor, tipo, status)
+        VALUES (?, ?, ?, ?)
+    """, (dados["nome"], dados["setor"], dados["tipo"], dados["status"]))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"mensagem": "Equipamento cadastrado com sucesso"}), 201
+
+
+@bp.route("/equipamentos", methods=["GET"])
+def get_equipamentos():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM equipamentos")
+    rows = cursor.fetchall()
+    conn.close() 
+
+    equipamentos = []
+    for row in rows:
+        equipamentos.append({
+            "id": row[0],
+            "nome": row[1],
+            "setor": row[2],
+            "tipo": row[3],
+            "status": row[4]
         })
 
-@bp.route("/echo", methods=["POST"])
-def echo():
-    dados = request.get_json()
-    return jsonify ({
-        "recebido": dados
-    })
+    return jsonify(equipamentos)
+
