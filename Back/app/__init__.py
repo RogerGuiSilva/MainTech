3f698a3e8d8772aafc516c041eb6bc22b6f7f7d7
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2026-02-02 12:41:34 -0300

    revert: remove CORS e arquivos locais do Frontend

--- Back/app/__init__.py
+++ Back/app/__init__.py
@@ -1,11 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
-
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:5500", "http://localhost:5500"]}})
-
 
     from app.routes import bp
     app.register_blueprint(bp)
