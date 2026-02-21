de0205fe4966a36d7e2ebf0eddf2a8964829d985
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2026-02-21 17:22:33 -0300

    mudando o visual

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
       <section className="home_hero">
         <img
           className="home_image"
-          src="/img/home-page.png"
+          src="/img/image.jpg"
           alt="Máquinas e equipamentos industriais"
         />
       </section>
@@ -26,19 +26,19 @@ export default function Home() {
         <Link className="card" to="/maquinas">
         <img src="/img/maquina.png" className="card_icon" />
           <h2>Máquinas</h2>
-          <p>Visualize e gerencie máquinas cadastradas.</p>
+          <p>Maquinas Cadastradas</p>
         </Link>
 
         <Link className="card" to="/falhas">
         <img src="/img/falhas.png" className="card_icon" />
           <h2>Falhas</h2>
-          <p>Acompanhe ocorrências e histórico de falhas.</p>
+          <p>ocorrências e histórico de falhas</p>
         </Link>
 
         <Link className="card" to="/equipamentos">
         <img src="/img/equipamento.png" className="card_icon" />
           <h2>Equipamentos</h2>
-          <p>Controle de equipamentos e componentes.</p>
+          <p>Equipamentos Cadastrados</p>
         </Link>
 
       </section>
